2945b4cd8644ada264d8ef653e803979ae8646c4
Muhammad Saim <141134993+saim-x@users.noreply.github.com>
2024-06-07 00:49:30 +0500

    Time: 19 ms (98.17%) | Memory: 21.2 MB (91.00%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    int minEatingSpeed(std::vector<int>& piles, int h) {
+        int left = 1;
+        int right = *std::max_element(piles.begin(), piles.end());
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (canFinish(piles, h, mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        return left;
+    }
+    
+private:
+    bool canFinish(const std::vector<int>& piles, int h, int k) {
+        int hours = 0;
+        for (const int& pile : piles) {
+            hours += (pile + k - 1) / k; 
+        }
+        return hours <= h;
+    }
+};
