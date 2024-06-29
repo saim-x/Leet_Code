150c021e5d4bb98e9f785eb5ef5a8f1333df856d
Muhammad Saim <141134993+saim-x@users.noreply.github.com>
2024-06-29 14:21:37 +0500

    Time: 21 ms (5.51%) | Memory: 10.7 MB (91.94%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ans;
+        for (int i = 0; i < nums1.size(); ++i) {
+            for (int j = 0; j < nums2.size() - 1; ++j) {
+
+                if (nums1[i] == nums2[j]) {
+                    // if (nums1[i] < nums2[j + 1]) {
+                    //     ans.push_back(nums2[j + 1]);
+                    // } else {
+                    //     ans.push_back(-1);
+                    // }
+                    bool flag = false;
+                    for (int k = j + 1; k < nums2.size(); k++) {
+                        if (nums1[i] < nums2[k]) {
+                            ans.push_back(nums2[k]);
+                            flag = true;
+                            break;
+                        }
+                    }
+                    if (!flag)
+                        ans.push_back(-1);
+                }
+            }
+            if (nums1[i] == nums2[nums2.size() - 1]) {
+                ans.push_back(-1);
+            }
+        }
+        return ans;
+    }
+};
