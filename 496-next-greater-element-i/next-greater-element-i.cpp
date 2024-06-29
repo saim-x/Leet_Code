dd08d3452be69fce9fadfba688ce9b5209dd1556
Muhammad Saim <141134993+saim-x@users.noreply.github.com>
2024-06-29 14:22:19 +0500

    Time: 22 ms (5.51%) | Memory: 10.8 MB (84.10%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -4,7 +4,6 @@ public:
         vector<int> ans;
         for (int i = 0; i < nums1.size(); ++i) {
             for (int j = 0; j < nums2.size() - 1; ++j) {
-
                 if (nums1[i] == nums2[j]) {
                     bool flag = false;
                     for (int k = j + 1; k < nums2.size(); k++) {
