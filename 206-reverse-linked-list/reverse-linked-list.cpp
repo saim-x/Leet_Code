95211891945c17bd86faa85538c19e906fb6f97e
Muhammad Saim <141134993+saim-x@users.noreply.github.com>
2024-07-06 14:56:27 +0500

    Time: 8 ms (17.15%) | Memory: 11.5 MB (67.85%) - LeetSync

---
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -0,0 +1,25 @@
+/**
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* next = nullptr;
+
+        while (head != NULL) {
+            next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;
+        }
+        head = prev;
+        return head;
+    }
+};
